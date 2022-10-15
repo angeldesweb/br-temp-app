4b4f6f746a9486d75d6aafe554fccb80ff6e08a5
angeldesweb <angeldesweb@gmail.com>
2022-10-15 00:08:14 -0430

    fix:optimizeDeps excluding notyf

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,4 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 
-declare module 'notyf';
 declare module '@beyonk\svelte-mapbox';
-declare module 'firebase';
-declare module 'felte';
